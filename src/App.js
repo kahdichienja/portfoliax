a6ed40d3f8f7af5b0fed7502f1a2f2e865ce6a53
kahdichienja <agooclinton@gmail.com>
2022-01-02 15:10:02 +0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import React from 'react';
-
-// import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
-// import { CTA, Brand, Navbar } from './components';
-
-import './App.css';
+import { Navbar } from "./components";
 
 const App = () => (
-  <div className="App">
-    <div className="gradient__bg">
-      {/* <Navbar />
-      <Header /> */}
+  <div className="min-h-screen">
+    <div className="gradient-bg-welcome">
+      <Navbar />
+      {/* <Welcome /> */}
     </div>
-    {/* <Brand />
-    <WhatGPT3 />
-    <Features />
-    <Possibility />
-    <CTA />
-    <Blog />
+    {/* <Services />
+    <Transactions />
     <Footer /> */}
   </div>
 );
